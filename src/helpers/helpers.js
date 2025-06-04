8ab4fa3713a727ebf81a90aa36794f68320942cd
DiShiro <q15112007@gmail.com>
2025-06-04 13:38:02 +0300

    jhlisdfdvfh commit

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,87 +1,85 @@
 export const calcNums = (num1, num2, operator) => {
     switch (operator) {
       case '+':
-        return num1 + num2
+        return num1 + num2;
   
       case '-':
-        return num1 - num2
+        return num1 - num2;
   
       case '*':
-        return num1 * num2
-    }
-  }
-  
-  export const generationMathOperator = () => {
-    const num = Math.floor(Math.random() * 3) 
-    switch (num) {
-      case 0:
-        return '+'
-  
-      case 1:
-        return '-'
+        return num1 * num2;
   
       default:
-        return '*'
+        throw new Error(`Unknown operator: ${operator}`);
     }
-  }
+  };
+  
+  export const generationMathOperator = () => {
+    const num = Math.floor(Math.random() * 3); 
+    switch (num) {
+      case 0:
+        return '+';
+  
+      case 1:
+        return '-';
+  
+      default:
+        return '*';
+    }
+  };
   
   export const isEven = (num) => {
-    if (num % 2 === 0) {
-      return true
-    }
-  
-    return false
-  }
+    return num % 2 === 0;
+  };
   
   export const gcd = (a, b) => {
-    a = Math.abs(a)
-    b = Math.abs(b)
+    a = Math.abs(a);
+    b = Math.abs(b);
   
     while (b !== 0) {
-      const temp = b
-      b = a % b
-      a = temp
+      const temp = b;
+      b = a % b;
+      a = temp;
     }
   
-    return a
-  }
+    return a;
+  };
   
   export const generationNumber = () => {
-    return Math.floor(Math.random() * 101) 
-  }
+    return Math.floor(Math.random() * 101);
+  };
   
   export const generationArithmeticProgression = () => {
-    const lengthProgression = Math.floor(Math.random() * (20 - 5 + 1)) + 5 // nosonar - not used for security purposes
+    const lengthProgression = Math.floor(Math.random() * (20 - 5 + 1)) + 5; // nosonar - not used for security purposes
+    const stepProgression = Math.floor(Math.random() * (10 - 1 + 1)) + 1; // nosonar - not used for security purposes
+    const start = Math.floor(Math.random() * 100);
+    let current = start;
   
-    const stepProgression = Math.floor(Math.random() * (10 - 1 + 1)) + 1 // nosonar - not used for security purposes
-  
-    const start = Math.floor(Math.random() * 100) 
-    let current = start
-  
-    const newArr = []
+    const newArr = [];
   
     for (let i = 0; i < lengthProgression; i += 1) {
-      newArr.push(current)
-      current += stepProgression
+      newArr.push(current);
+      current += stepProgression;
     }
   
-    return newArr
-  }
+    return newArr;
+  };
   
   export const replaceRandomWithDots = (arr) => {
-    const positionPoints = Math.floor(Math.random() * arr.length) // nosonar - not used for security purposes
-    const newArr = arr.map((item, index) => (index !== positionPoints ? item : '..'))
+    const positionPoints = Math.floor(Math.random() * arr.length); // nosonar - not used for security purposes
+    const newArr = arr.map((item, index) => (index !== positionPoints ? item : '..'));
   
-    return [newArr, arr[positionPoints]]
-  }
+    return [newArr, arr[positionPoints]];
+  };
   
   export const isPrime = (num) => {
-    if (num <= 1) return false 
-    if (num === 2) return true 
+    if (num <= 1) return false; 
+    if (num === 2) return true; 
   
     for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) return false
+      if (num % i === 0) return false;
     }
   
-    return true
-  }
+    return true;
+  };
+  
